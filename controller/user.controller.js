a053d88a49facac2fbce168c7e559299b74151e4
chandrakant rana <chandresh@logicwind.com>
2021-02-28 09:54:08 +0530

    Changed in update_user api

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,5 @@
 const models = require("../models");
+const { Op } = require('sequelize');
 
 exports.UPDATE_USER = async (req, res) => {
   let client = req.body;
@@ -7,10 +8,25 @@ exports.UPDATE_USER = async (req, res) => {
       where: {
         email: client.email
       }
-    });
-    // result.salt = undefined;
-    // result.hashed_password = undefined;
-    res.json(result);
+    });    
+    if (result && result.length > 0 && result[0] === 1) {      
+      let user = await models.Client.findOne({
+        attributes: { exclude: ['deletedAt'] },
+        where: { email: client.email, deletedAt: { [Op.eq]: null } }
+      });
+      if (!user) {
+        return res.status(400).json({
+          error: "User with this email does not exist. Please Singup"
+        })
+      }
+      // do not send salt in response
+      user.salt = undefined;
+      user.hashed_password = undefined;
+      let data = { "user": user };
+      console.log("result--user: ", res.json(data))
+      return res.json(data);
+    }    
+    return res.json(result);
   } catch (error) {
     res.status(400).json({ error: { message: error.message } });
   }
